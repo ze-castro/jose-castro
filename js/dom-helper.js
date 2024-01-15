b4e3185c418ca9ac2602b05c4e66873dda929f42
Zé Castro <a20351@alunos.ipca.pt>
2024-01-15 20:01:31 +0000

    very important updates

--- js/dom-helper.js
+++ js/dom-helper.js
@@ -3,7 +3,7 @@ function toggleDescription(box) {
   box.classList.toggle('expanded');
 }
 
-//scroll to section
+// scroll to section
 function scrollToSection(sectionName) {
   var section = document.getElementById(sectionName);
   var sectionOffset = section.offsetTop;
@@ -13,4 +13,15 @@ function scrollToSection(sectionName) {
     top: sectionOffset - offset,
     behavior: 'smooth',
   });
+}
+
+// click on "get started" button to write a description in textarea
+const textDescriptions = ['I need a website to promote my work',
+'I need a way to sell my products online', 
+'I need to manage my business internally', 
+'I need an bespoke online store for my business'];
+
+function writeDescription(num) {
+  document.getElementById('textarea').value = textDescriptions[num];
+  scrollToSection('contact');
 }

e8de717bca809e5aa47c411c18a0988d3363c728
Zé Castro <a20351@alunos.ipca.pt>
2024-07-07 16:54:32 +0100

    updated faq

--- js/dom-helper.js
+++ js/dom-helper.js
@@ -1,7 +1,35 @@
 //// TOOLS ////
 // expand faq boxes
 function toggleDescription(box) {
+  // get all the boxes
+  var boxes = document.querySelectorAll('.box');
+
+  // add border-top to all boxes
+  boxes.forEach((b) => {
+    b.style.borderTop = '1px solid var(--light-gray)';
+  });
+
+  // check if the box is expanded
+  if (box.classList.contains('expanded')) {
+    box.classList.remove('expanded');
+    return;
+  }
+
+  // the border-top of the next sibling must be removed
+  var nextSibling = box.nextElementSibling;
+  if (nextSibling) {
+    nextSibling.style.borderTop = 'none';
+  }
+
+  // toggle the expanded class
   box.classList.toggle('expanded');
+
+  // if any other box is expanded, close it
+  boxes.forEach((b) => {
+    if (b !== box) {
+      b.classList.remove('expanded');
+    }
+  });
 }
 
 // scroll to section
@@ -19,4 +47,4 @@ function scrollToSection(sectionName) {
 // get the current year
 const thisYear = new Date().getFullYear();
 
-export { thisYear, toggleDescription, scrollToSection};
+export { thisYear, toggleDescription, scrollToSection };
